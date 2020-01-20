4a06197134bdcf3e83ee688215b84fe55e1511d8
Nallely <nallely.nnc@gmail.com>
2020-01-19 23:10:30 -0600

    Ejercicio Num. Romanos

---
+++ romanos3.js
@@ -0,0 +1,22 @@
+function romanos(N){
+    var nums = { 1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40:"XL", 50: "L", 90: "XC", 400: "CD", 500: "D", 900: "CM"};
+      var resp = "";
+      let n= N;
+    let my_index = Object.keys(nums).length;
+    let my_keys = Object.keys(nums);
+  
+      while(n>0){
+        if (my_keys[my_index]<=n)
+          {
+          nkey = my_keys[my_index];
+          resp = resp.concat(nums[nkey]);
+          n = n - my_keys[my_index];
+          }
+          else{
+            my_index = my_index - 1;
+          }
+      }
+      return resp;
+  };
+  
+  console.log(romanos(57));
